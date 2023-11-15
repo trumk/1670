5ad9253f939ddf5a391972d3ac341f8e7b0215cb
trumk <nguyentrungtest2292003@gmail.com>
2023-11-15 22:44:37 +0700

    Add project files.

---
+++ buoi1/Data/buoi1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using buoi1.Models;
+
+namespace buoi1.Data
+{
+    public class buoi1Context : DbContext
+    {
+        public buoi1Context (DbContextOptions<buoi1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<buoi1.Models.Ticket> Ticket { get; set; } = default!;
+    }
+}
---
+++ buoi1/Models/Ticket.cs
@@ -0,0 +1,12 @@
+﻿namespace buoi1.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public decimal? Price { get; set; }
+        public string? Genre { get; set; }
+    }
+}
