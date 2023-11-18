a987926d92e7257d35098f1870b1efc43500e109
trumk <nguyentrungtest2292003@gmail.com>
2023-11-18 11:44:34 +0700

    validation

--- buoi1/Models/Ticket.cs
+++ buoi1/Models/Ticket.cs
@@ -1,13 +1,21 @@
-﻿namespace buoi1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace buoi1.Models
 {
     public class Ticket
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
         public string? Title { get; set; }
+        [RegularExpression(@"[A-Z]+[a-zA-Z0-9""'\s]*$")]
         public string Description { get; set; }
+        [Display(Name = "Release Date")]
         public DateTime? ReleaseDate { get; set; }
-        public decimal? Price { get; set; }
+        [Range(1, 1000)]
+        public decimal Price { get; set; }
         public string? Genre { get; set; }
+        [RegularExpression(@"[A-Z]+[a-zA-Z\s]*$")]
         public string? Rating { get; set; }
     }
 }
--- buoi2/Models/Ticket.cs
+++ buoi2/Models/Ticket.cs
@@ -6,16 +6,16 @@ namespace buoi2.Models
     {
         public int Id { get; set; }
         [Required]
-        [StringLength(100, MinimumLength = 3)]
+        [StringLength(60, MinimumLength = 3)]
         public string? Title { get; set; }
         [RegularExpression(@"[A-Z]+[a-zA-Z0-9""'\s]*$")]
         public string Description { get; set; }
-        [Display(Name = "Release Ddate")]
+        [Display(Name = "Release Date")]
         public DateTime? ReleaseDate { get; set; }
-        [Range(1,1000)]
-        public decimal? Price { get; set; }
+        [Range(1, 1200)]
+        public decimal Price { get; set; }
         public string? Genre { get; set; }
         [RegularExpression(@"[A-Z]+[a-zA-Z\s]*$")]
-        public string? Rating {  get; set; }
+        public string? Rating { get; set; }
     }
 }
