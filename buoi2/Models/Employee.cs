06e96e9ec9b38ff02911a128aeb7cffea64e2f63
trumk <nguyentrungtest2292003@gmail.com>
2023-11-21 20:24:39 +0700

    model

---
+++ buoi2/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace buoi2.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public int Salary { get; set; }
+        public string ProfilePicture { get; set; }
+        public Employee()
+        {
+            FullName = FirstName + " " + LastName;
+        }
+        [NotMapped]
+        public string FullName { get; set; }
+        [NotMapped]
+        public IFormFile ProfileImage { get; set; }
+    }
+}
