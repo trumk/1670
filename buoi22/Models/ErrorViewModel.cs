cf4bc6e926f7a3b2750ff5419c4b18600f89aa7c
trumk <nguyentrungtest2292003@gmail.com>
2023-11-21 08:08:46 +0700

    buoi2

---
+++ buoi22/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace buoi22.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
