153689f067ca9058e2c9c347c1cb008cf00c8536
trumk <nguyentrungtest2292003@gmail.com>
2023-11-21 09:00:06 +0700

    setup homecontroller

--- buoi22/Controllers/HomeController.cs
+++ buoi22/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 ﻿using buoi22.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using buoi22.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace buoi22.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly buoi22Context dbContext;
+        private readonly IWebHostEnvironment webHostEnvironment;
+        public HomeController(buoi22Context dbContext, IWebHostEnvironment webHostEnvironment)
         {
-            _logger = logger;
+            this.dbContext = dbContext;
+            this.webHostEnvironment = webHostEnvironment;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var employee = await dbContext.Employee.ToListAsync();
+            return View(employee);
         }
 
         public IActionResult Privacy()
@@ -27,6 +32,34 @@ namespace buoi22.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult New() { 
+        return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> New(Employee model)
+        {
+            if(ModelState.IsValid)
+            {
+                string uniqueFilename = UploadedFile(model);
+            }
+            return View();
+        }
+        private string UploadedFile(Employee model)
+        {
+            string uniqueFileName = null;
+            if (model.ProfileImage != null)
+            {
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ProfileImage.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.ProfileImage.CopyTo(fileStream);
+                }
+            }
+            return uniqueFileName;
         }
     }
 }
