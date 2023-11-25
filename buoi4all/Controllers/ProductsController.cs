034479300af857ec0c9d93a3fd8f029753629cd5
trumk <nguyentrungtest2292003@gmail.com>
2023-11-25 19:57:58 +0700

    upp

--- buoi4all/Controllers/ProductsController.cs
+++ buoi4all/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using buoi4all.Data;
 using buoi4all.Models;
+using Newtonsoft.Json;
 
 namespace buoi4all.Controllers
 {
@@ -22,9 +23,9 @@ namespace buoi4all.Controllers
         // GET: Products
         public async Task<IActionResult> Index()
         {
-              return _context.Product != null ? 
-                          View(await _context.Product.ToListAsync()) :
-                          Problem("Entity set 'buoi4allContext.Product'  is null.");
+            return _context.Product != null ?
+                        View(await _context.Product.ToListAsync()) :
+                        Problem("Entity set 'buoi4allContext.Product'  is null.");
         }
 
         // GET: Products/Details/5
@@ -150,14 +151,123 @@ namespace buoi4all.Controllers
             {
                 _context.Product.Remove(product);
             }
-            
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool ProductExists(int id)
         {
-          return (_context.Product?.Any(e => e.ProductID == id)).GetValueOrDefault();
+            return (_context.Product?.Any(e => e.ProductID == id)).GetValueOrDefault();
+        }
+        public IActionResult IndexCart()
+        {
+            var _products = GetAllProducts();
+            ViewBag.products = _products;
+            return View();
+        }
+        public List<Product> GetAllProducts()
+        {
+            return _context.Product.ToList();
+        }
+        public Product GetDetailProduct(int id)
+        {
+            var product = _context.Product.Find(id);
+            return product;
+        }
+        public IActionResult AddCart(int id)
+        {
+            var cart = HttpContext.Session.GetString("cart");
+            if (cart == null)
+            {
+                var product = GetDetailProduct(id);
+                List<Cart> ListCart = new List<Cart>()
+                {
+                 new Cart
+                 {
+                 Product = product,
+                 Quantity = 1
+                 }
+         };
+                HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(ListCart));
+            }
+            else
+            {
+                List<Cart> dataCart = JsonConvert.DeserializeObject<List<Cart>>(cart);
+                bool check = true;
+                for (int i = 0; i < dataCart.Count; i++)
+                {
+                    if (dataCart[i].Product.ProductID == id)
+                    {
+                        dataCart[i].Quantity++;
+                        check = false;
+                    }
+                }
+                if (check)
+                {
+                    dataCart.Add(new Cart
+                    {
+                        Product = GetDetailProduct(id),
+                        Quantity = 1
+                    });
+                }
+                HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(dataCart));
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult UpdateCart(int id, int quantity)
+        {
+            var cart = HttpContext.Session.GetString("cart");
+            if (cart != null)
+            {
+                List<Cart> dataCart = JsonConvert.DeserializeObject<List<Cart>>(cart);
+                if (quantity > 0)
+                {
+                    for (int i = 0; i < dataCart.Count; i++)
+                    {
+                        if (dataCart[i].Product.ProductID == id)
+                        {
+                            dataCart[i].Quantity = quantity;
+                        }
+                    }
+                    HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(dataCart));
+                }
+                return Ok(quantity);
+            }
+            return BadRequest();
+        }
+        public IActionResult DeleteCart(int id)
+        {
+            var cart = HttpContext.Session.GetString("cart");
+            if (cart != null)
+            {
+                List<Cart> dataCart = JsonConvert.DeserializeObject<List<Cart>>(cart);
+                for (int i = 0; i < dataCart.Count; i++)
+                {
+                    if (dataCart[i].Product.ProductID == id)
+                    {
+                        dataCart.RemoveAt(i);
+                    }
+                }
+                HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(dataCart));
+                return RedirectToAction(nameof(ListCart));
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult ListCart()
+        {
+            var cart = HttpContext.Session.GetString("cart");
+            if (cart != null)
+            {
+                List<Cart> dataCart = JsonConvert.DeserializeObject<List<Cart>>(cart);
+                if (dataCart.Count > 0)
+                {
+                    ViewBag.carts = dataCart;
+                    return View();
+                }
+                return RedirectToAction(nameof(ListCart));
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
