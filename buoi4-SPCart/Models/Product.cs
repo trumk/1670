308e1db0ba9a8c3a74226bef239babf4f527f94c
trumk <nguyentrungtest2292003@gmail.com>
2023-11-25 19:24:37 +0700

    change model

--- buoi4-SPCart/Models/Product.cs
+++ buoi4-SPCart/Models/Product.cs
@@ -1,11 +1,16 @@
-﻿namespace buoi4_SPCart.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace buoi4_SPCart.Models
 {
     public class Product
     {
         public int ProductID { get; set; }
         public string Title { get; set; }
         public string Detail { get; set; }
-        public string UrlImage { get; set; }
         public decimal Price { get; set; }
+        public string? Picture { get; set; }
+
+        [NotMapped]
+        public IFormFile Image { get; set; }
     }
 }
